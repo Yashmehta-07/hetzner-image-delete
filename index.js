357e76ea2ce9a545425cc65cb786c137d0a14830
Yashmehta-07 <yash.mehta@remotestate.com>
2024-07-23 19:36:55 +0530

    add 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = 8000;
 const requestHandler = (request, response) => {
 console.log(request.url);
-response.end('Hello Node.js Server!');
+response.end('Hello Node.js Server2!');
 };
 const server = http.createServer(requestHandler);
 server.listen(port, (err) => {
